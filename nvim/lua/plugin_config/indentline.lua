4fd5d72195f05fc715c8ea65e8bdd8ce7da33687
phu0ngng <bichphuong1204@gmail.com>
2023-12-05 16:39:18 -0500

    updated indentline

--- nvim/lua/plugin_config/indentline.lua
+++ nvim/lua/plugin_config/indentline.lua
@@ -1,10 +1,15 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, ibl = pcall(require, "ibl")
 if not status_ok then
 	return
 end
 
-vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-vim.g.indent_blankline_filetype_exclude = {
+local status_ok_2, icons = pcall(require, "plugin_config.icons")
+if not status_ok_2 then
+	return
+end
+
+vim.g.ibl_buftype_exclude = { "terminal", "nofile" }
+vim.g.ibl_filetype_exclude = {
 	"help",
 	"startify",
 	"dashboard",
@@ -12,59 +17,19 @@ vim.g.indent_blankline_filetype_exclude = {
 	"neogitstatus",
 	"NvimTree",
 	"Trouble",
+  "text",
 }
-vim.g.indentLine_enabled = 1
--- vim.g.indent_blankline_char = "│"
-vim.g.indent_blankline_char = "▏"
--- vim.g.indent_blankline_char = "▎"
-vim.g.indent_blankline_show_trailing_blankline_indent = false
-vim.g.indent_blankline_show_first_indent_level = true
-vim.g.indent_blankline_use_treesitter = true
-vim.g.indent_blankline_show_current_context = true
-vim.g.indent_blankline_context_patterns = {
-	"class",
-	"return",
-	"function",
-	"method",
-	"^if",
-	"^while",
-	"jsx_element",
-	"^for",
-	"^object",
-	"^table",
-	"block",
-	"arguments",
-	"if_statement",
-	"else_clause",
-	"jsx_element",
-	"jsx_self_closing_element",
-	"try_statement",
-	"catch_clause",
-	"import_statement",
-	"operation_type",
-}
--- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
-vim.wo.colorcolumn = "99999"
+vim.g.ibl_char = icons.ui.LineMiddle
+vim.g.ibl_context_char = icons.ui.LineMiddle
+vim.g.ibl_show_trailing_blankline_indent = false
+vim.g.ibl_show_first_indent_level = true
+vim.g.ibl_use_treesitter = true
+vim.g.ibl_show_current_context = true
 
--- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
--- vim.opt.list = true
--- vim.opt.listchars:append "space:⋅"
--- vim.opt.listchars:append "space:"
--- vim.opt.listchars:append "eol:↴"
-
-indent_blankline.setup({
+ibl.setup({
 	-- show_end_of_line = true,
 	-- space_char_blankline = " ",
-	show_current_context = true,
 	-- show_current_context_start = true,
-	-- char_highlight_list = {
-	--   "IndentBlanklineIndent1",
-	--   "IndentBlanklineIndent2",
-	--   "IndentBlanklineIndent3",
-	-- },
 })
+
+vim.keymap.set('n', '<C-l>', ':IBLToggle<Enter>')
