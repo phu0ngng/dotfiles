70419551972ffcba7de400a95ff7ca948e7f1d42
Phuong Nguyen <bichphuong1204@gmail.com>
2024-04-15 11:47:39 -0700

    python path

--- install.sh
+++ install.sh
@@ -195,7 +195,7 @@ fi
 # Install nvim plugin
 mkdir -p ~/.config/nvim
 cp -r $DotFilesDir/nvim/* ~/.config/nvim/
-echo "vim.g.python3_host_prog=$(which python3)" >> ~/.config/nvim/lua/options.lua
+echo "'vim.g.python3_host_prog=$(which python3)'" >> ~/.config/nvim/lua/options.lua
 #
 pip3 install neovim flake8 black prettier
 pip3 install "python-lsp-server[all]" -U setuptools cpplint
