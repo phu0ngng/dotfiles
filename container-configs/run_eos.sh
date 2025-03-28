77e51db71cc644f715592fb67543ca2a6f9967d2
Phuong Nguyen <phuonguyen@nvidia.com>
2025-03-28 08:37:39 -0700

    update run scripts
    Signed-off-by: Phuong Nguyen <phuonguyen@nvidia.com>

--- container-configs/run_eos.sh
+++ container-configs/run_eos.sh
@@ -12,23 +12,43 @@ case "$IMAGE" in
     IMAGE_LINK="ghcr.io/nvidia/jax:jax"
     ;;
   "torch")
-    IMAGE_LINK="gitlab-master.nvidia.com/dl/dgx/pytorch:master-py3-devel"
+    IMAGE_LINK="gitlab-master.nvidia.com/dl/dgx/pytorch:main-py3-devel"
     ;;
   "bw-jax")
-    IMAGE_LINK="gitlab-master.nvidia.com:5005/dl/dgx/jax:blackwell-jax"
+    IMAGE_LINK="gitlab-master.nvidia.com:5005/dl/dgx/jax:jax"
     ;;
   "bw-torch")
-    IMAGE_LINK="gitlab-master.nvidia.com/dl/dgx/pytorch:blackwell-py3-devel"
+    IMAGE_LINK="gitlab-master.nvidia.com/dl/dgx/pytorch:main-py3-devel"
     ;;
   *)
     echo "Invalid IMAGE"
     ;;
 esac
 
-srun -A coreai_dlfw_dev -N1 -p batch\
-    -t 4:00:00 -J coreai_dlfw_dev-te:te_jax\
-    --container-image=${IMAGE_LINK}\
-    --container-name=${IMAGE}-ct\
-    --container-mounts "/home/phuonguyen/te-${TE}"\
-    --container-workdir="/home/phuonguyen/te-${TE}"\
+
+SAVED_IMAGE=`pwd`/images/${IMAGE}.sqsh
+
+mkdir -p "`pwd`/images"
+
+PARTITION="batch"
+ACCOUNT="coreai_dlfw_dev"
+
+if [ -f "$SAVED_IMAGE" ]; then
+  srun -A $ACCOUNT -N 1 -p $PARTITION -t 4:00:00 \
+    -J "$ACCOUNT-te.test-${IMAGE}" \
+    --container-image=${SAVED_IMAGE} \
+    --container-name=${IMAGE}-ct \
+    --container-mounts "${WORKSPACE}/te-${TE}":"/home/phuonguyen/te-${TE}" \
+    --container-workdir="/home/phuonguyen/te-${TE}" \
     --no-container-remap-root --pty bash
+else
+  # If SAVED_IMAGE doesn't exist, fetch a new image from the IMAGE_LINK
+  srun -A $ACCOUNT -N 1 -p $PARTITION -t 4:00:00 \
+    -J "$ACCOUNT-te.test-${IMAGE}" \
+    --container-image=${IMAGE_LINK} \
+    --container-name=${IMAGE}-ct \
+    --container-save="${SAVED_IMAGE}" \
+    --container-mounts "${WORKSPACE}/te-${TE}":"/home/phuonguyen/te-${TE}" \
+    --container-workdir="/home/phuonguyen/te-${TE}" \
+    --no-container-remap-root --pty bash
+fi
