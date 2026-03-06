47d10e8dd1f2152600aba5e5c1977f1f7932aaf8
Phuong Nguyen <bichphuong1204@gmail.com>
2026-03-06 08:07:33 -0800

    map claude to container
    Signed-off-by: Phuong Nguyen <bichphuong1204@gmail.com>

--- container-configs/run_eos.sh
+++ container-configs/run_eos.sh
@@ -1,54 +1,12 @@
-#/bin/bash
-#
-# TE_MNT="/home/phuonguyen/te-jax:/home/phuonguyen/te-jax"
-# DATA_MNT="/lustre/fsw/coreai_dlfw_dev/mingh:/opt/llama"
+#!/bin/bash
 
-if [ -z "${TE}" ]; then 
-  TE="${IMAGE}"
-fi
-
-case "$IMAGE" in
-  "jax")
-    IMAGE_LINK="ghcr.io/nvidia/jax:jax"
-    ;;
-  "torch")
-    IMAGE_LINK="gitlab-master.nvidia.com/dl/dgx/pytorch:main-py3-devel"
-    ;;
-  "bw-jax")
-    IMAGE_LINK="gitlab-master.nvidia.com:5005/dl/dgx/jax:jax"
-    ;;
-  "bw-torch")
-    IMAGE_LINK="gitlab-master.nvidia.com/dl/dgx/pytorch:main-py3-devel"
-    ;;
-  *)
-    echo "Invalid IMAGE"
-    ;;
-esac
-
-
-SAVED_IMAGE=`pwd`/images/${IMAGE}.sqsh
-
-mkdir -p "`pwd`/images"
-
-PARTITION="batch"
-ACCOUNT="coreai_dlfw_dev"
-
-if [ -f "$SAVED_IMAGE" ]; then
-  srun -A $ACCOUNT -N 1 -p $PARTITION -t 4:00:00 \
-    -J "$ACCOUNT-te.test-${IMAGE}" \
-    --container-image=${SAVED_IMAGE} \
-    --container-name=${IMAGE}-ct \
-    --container-mounts "${WORKSPACE}/te-${TE}":"/home/phuonguyen/te-${TE}" \
-    --container-workdir="/home/phuonguyen/te-${TE}" \
-    --no-container-remap-root --pty bash
-else
-  # If SAVED_IMAGE doesn't exist, fetch a new image from the IMAGE_LINK
-  srun -A $ACCOUNT -N 1 -p $PARTITION -t 4:00:00 \
-    -J "$ACCOUNT-te.test-${IMAGE}" \
-    --container-image=${IMAGE_LINK} \
-    --container-name=${IMAGE}-ct \
-    --container-save="${SAVED_IMAGE}" \
-    --container-mounts "${WORKSPACE}/te-${TE}":"/home/phuonguyen/te-${TE}" \
-    --container-workdir="/home/phuonguyen/te-${TE}" \
-    --no-container-remap-root --pty bash
-fi
+srun -A coreai_dlfw_dev -N1 -p batch\
+    -t 4:00:00 -J coreai_dlfw_dev-te:te_maxtext\
+    --container-image="ghcr.io/nvidia/jax:maxtext-2026-03-05"\
+    --container-name=te_ct\
+    --container-mounts "/lustre/fsw/coreai_dlfw_dev/phuong:/scratch,/home/phuonguyen/te,/home/phuonguyen/.local/share/claude,/home/phuonguyen/.local/bin/claude,/home/phuonguyen/.claude,/home/phuonguyen/.claude.json,/home/phuonguyen/.config,/home/phuonguyen/.cache/claude,/home/phuonguyen/maxtext,/home/phuonguyen/.ssh,/home/phuonguyen/te"\
+    --container-workdir="/home/phuonguyen/te"\
+    --container-writable \
+    --no-container-remap-root\
+    --export=ALL \
+    --pty bash -c 'export HOME=/home/phuonguyen && export PATH=/home/phuonguyen/.local/bin:$PATH && apt-get update && apt-get install -y bubblewrap socat && pip install ninja pybind11 pytest && exec bash --rcfile <(echo "export HOME=/home/phuonguyen; export PATH=/home/phuonguyen/.local/bin:\$PATH; alias teinstall=\"pip install --no-build-isolation -e .\"")'
