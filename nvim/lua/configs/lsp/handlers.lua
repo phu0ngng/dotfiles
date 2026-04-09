cc7f084daa0c075ddbadf1bc075c291521966359
Phuong Nguyen <bichphuong1204@gmail.com>
2026-04-08 21:24:16 -0700

    fix deprecated vim.lsp.with() calls for nvim v0.12

--- nvim/lua/configs/lsp/handlers.lua
+++ nvim/lua/configs/lsp/handlers.lua
@@ -33,13 +33,15 @@ M.setup = function()
 		},
 	})
 
-	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-		border = "rounded",
-	})
+	vim.lsp.handlers["textDocument/hover"] = function(err, result, ctx, config)
+		config = vim.tbl_extend("force", config or {}, { border = "rounded" })
+		vim.lsp.handlers.hover(err, result, ctx, config)
+	end
 
-	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-		border = "rounded",
-	})
+	vim.lsp.handlers["textDocument/signatureHelp"] = function(err, result, ctx, config)
+		config = vim.tbl_extend("force", config or {}, { border = "rounded" })
+		vim.lsp.handlers.signature_help(err, result, ctx, config)
+	end
 end
 
 local function lsp_keymaps(bufnr)
